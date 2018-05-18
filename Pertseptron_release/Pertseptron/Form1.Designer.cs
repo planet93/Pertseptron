be616669b33bfa23e52340c22f2a3b28e5696911
planet93 <serj.planet@mail.ru>
2018-05-18 10:36:15 +0300

    Add files via upload

---
+++ Pertseptron_release/Pertseptron/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace Pertseptron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_learn = new System.Windows.Forms.Button();
+            this.button_recognition = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox_new_weight = new System.Windows.Forms.CheckBox();
+            this.checkBox_exit_learn = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_learn
+            // 
+            this.button_learn.Enabled = false;
+            this.button_learn.Location = new System.Drawing.Point(12, 36);
+            this.button_learn.Name = "button_learn";
+            this.button_learn.Size = new System.Drawing.Size(126, 24);
+            this.button_learn.TabIndex = 0;
+            this.button_learn.Text = "Обучение";
+            this.button_learn.UseVisualStyleBackColor = true;
+            this.button_learn.Click += new System.EventHandler(this.button_learn_Click);
+            // 
+            // button_recognition
+            // 
+            this.button_recognition.Enabled = false;
+            this.button_recognition.Location = new System.Drawing.Point(234, 33);
+            this.button_recognition.Name = "button_recognition";
+            this.button_recognition.Size = new System.Drawing.Size(139, 24);
+            this.button_recognition.TabIndex = 1;
+            this.button_recognition.Text = "Распознавание";
+            this.button_recognition.UseVisualStyleBackColor = true;
+            this.button_recognition.Click += new System.EventHandler(this.button_recognition_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.openFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(577, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.openFileToolStripMenuItem.Text = "Открыть файл";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.Location = new System.Drawing.Point(159, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(54, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox2.Location = new System.Drawing.Point(391, 36);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(45, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "1";
+            // 
+            // checkBox_new_weight
+            // 
+            this.checkBox_new_weight.AutoSize = true;
+            this.checkBox_new_weight.Location = new System.Drawing.Point(400, 118);
+            this.checkBox_new_weight.Name = "checkBox_new_weight";
+            this.checkBox_new_weight.Size = new System.Drawing.Size(143, 17);
+            this.checkBox_new_weight.TabIndex = 5;
+            this.checkBox_new_weight.Text = "новые начальные веса";
+            this.checkBox_new_weight.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_exit_learn
+            // 
+            this.checkBox_exit_learn.AutoSize = true;
+            this.checkBox_exit_learn.Location = new System.Drawing.Point(400, 150);
+            this.checkBox_exit_learn.Name = "checkBox_exit_learn";
+            this.checkBox_exit_learn.Size = new System.Drawing.Size(130, 17);
+            this.checkBox_exit_learn.TabIndex = 6;
+            this.checkBox_exit_learn.Text = "завершить обучение";
+            this.checkBox_exit_learn.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(488, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Iter";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(13, 90);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(122, 13);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "Значение на эталонах:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(16, 122);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(143, 13);
+            this.label15.TabIndex = 9;
+            this.label15.Text = "Результат распознавания:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 204);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 118);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(219, 204);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 118);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(413, 204);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 118);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(16, 354);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 118);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(219, 354);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(140, 118);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(413, 354);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(140, 118);
+            this.textBox6.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Эталон 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Эталон 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Эталон 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Эталон 1 + шум";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Эталон 2 + шум";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(410, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Эталон 3 + шум";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 325);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "ЗАШУМЛЕННЫЕ ЭТАЛОНЫ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 484);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox_exit_learn);
+            this.Controls.Add(this.checkBox_new_weight);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button_recognition);
+            this.Controls.Add(this.button_learn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_learn;
+        private System.Windows.Forms.Button button_recognition;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox_new_weight;
+        private System.Windows.Forms.CheckBox checkBox_exit_learn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Pertseptron_release/Pertseptron/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Pertseptron
+{
+    public partial class Form1 : Form
+    {
+        const int NI = 10000;
+
+        string[] bytes_str = new string[6];
+
+        double[] w = new double[15];
+        double w0;
+
+        double[] an = new double[15];
+        double[] zn = new double[15];
+        double[] yn = new double[15];
+
+        int pattern; //номер распознаваемого эталона
+        int iter;
+
+        double[,] xx = new double[3, 15]; //выборка (эталоны)
+        double[,] xxn = new double[3, 15]; //зашумлённые
+
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double h(double a)
+        {
+            return (Math.Exp(a) - Math.Exp(-a)) / (Math.Exp(a) + Math.Exp(-a));
+        }
+        double s(double a)
+        {
+            return 1.0 / (1.0 + Math.Exp(-a));
+        }
+
+        void Init()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                w[i] = rnd.Next(0, 9) + 1;
+            }
+            w0 = 1;
+        }
+
+        double npfunc (double [,] xx, int n)
+        {
+            int i;
+            double sum=0;
+
+            for (i = 0; i < 15; i++)
+            {
+                sum = sum + w[i] * xx[n,i];
+            }
+            sum = sum + w0;
+            an[0] = sum;
+            zn[0] = h(an[0]);
+
+            return zn[0];
+        }
+
+        bool Stop()
+        {
+            bool flag = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (((i != pattern) && (npfunc(xx, i)) > -0.9) || ((i == pattern) && (npfunc(xx, i)) < 0.9))
+                {
+                    flag = false;
+                }
+            }
+
+            return flag;
+        }
+
+        double Mistake()
+        {
+            double sum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == pattern)
+                {
+                    sum = sum + (npfunc(xx, i) - 1.0) * (npfunc(xx, i) - 1.0);
+                }
+                else
+                {
+                    sum = sum + (npfunc(xx, i) + 1.0) * (npfunc(xx, i) + 1.0);
+                }
+            }
+
+            return sum;
+        }
+
+        void SimpleLearning()
+        {
+            int npattern;
+            double step, delta, E, Ed;
+
+            iter = 0;
+            step = 1.0;
+            delta = 0.001;
+
+            pattern = Convert.ToInt32(comboBox1.Text);
+
+            Init();
+
+            while (!Stop())
+            {
+                if (iter > NI)
+                {
+                    iter = 0;
+                    Init();
+                }
+
+                iter = iter + 1;
+
+                label7.Text = iter.ToString();
+                label7.Refresh();
+
+                npattern = rnd.Next(0, 3);
+
+                if (npattern == pattern)
+                {
+                    if (npfunc(xx, npattern) < 0.9)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            if (xx[pattern, i] == 1)
+                            {
+                                if (w[i] < 1000)
+                                {
+                                    w[i] = w[i] + step;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (npattern != pattern)
+                {
+                    if (npfunc(xx, npattern) > -0.9)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            if (xx[npattern, i] == 1)
+                            {
+                                if (w[i] > -1000)
+                                {
+                                    w[i] = w[i] - step;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                E = Mistake();
+                w0 = w0 + delta;
+                Ed = Mistake();
+                w0 = w0 - 10.0 * (Ed - E) / delta;
+                label12.Text = "Значение на эталонах: " + (npfunc(xx, 0).ToString()) + ";" + (npfunc(xx, 1).ToString()) + " ;" + (npfunc(xx, 2).ToString());
+                label12.Refresh();
+                Thread.Sleep(50);
+
+                if(checkBox_new_weight.Checked)
+                {
+                    checkBox_new_weight.Checked = false;
+                    iter = 0;
+                    Init();
+                }
+
+                if(checkBox_exit_learn.Checked)
+                {
+                    break;
+                }
+
+            }
+        }
+
+        private void button_learn_Click(object sender, EventArgs e)
+        {
+            SimpleLearning();
+        }
+
+        private void button_recognition_Click(object sender, EventArgs e)
+        {
+            pattern = Convert.ToInt32(comboBox1.Text);
+            label15.Text = "Результат распознавания:" + Convert.ToDouble(npfunc(xxn, Convert.ToInt32(comboBox2.Text) - 1));
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //Спрашиваем какой файл открыть
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string fileName = openFileDialog1.FileName;
+
+            
+            StreamReader sr = new StreamReader(fileName);
+            string text = sr.ReadToEnd();//Читаем весь текст в файле
+
+            //Разделяем весь текст на 6 строк
+            string [] raz = text.Split(new char[] { '\n' });
+
+            int i = 0;
+            foreach (string r in raz)
+            {
+                //Далее разобъем по столбцам
+                string[] column = r.Split(new char[] { ' ' });
+
+                foreach (string col in column)
+                {
+                    //Проверим полученный символ, убрав пробелы и проверим не пустое ли значение
+                    if (col.Trim() != "")
+                    {
+                        bytes_str[i] = col;
+                        i++;
+                    }
+                }
+            }
+            //Вызываем функцию, чтобы заполнить массивы и поля считанными данными
+            input_data();
+
+        }
+
+        void input_data()
+        {
+            int i, j;
+            string buf;
+
+            for (i = 1; i < 6; i++)
+            {
+                buf = "";
+                for (j = 1; j < 4; j++)
+                {
+                    buf = buf + bytes_str[0][((i - 1) * 3 + j) - 1];
+                }
+                textBox1.Text = textBox1.Text + buf + "\r\n";//Полученный результат записываем в первый текстбокс
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                xx[0, i] = Convert.ToDouble(char.GetNumericValue(bytes_str[0][i]));//Заполнем значение массива, преобразуя из символа к числу вещественного вида
+            }
+
+            for (i = 1; i < 6; i++)
+            {
+                buf = "";
+                for (j = 1; j < 4; j++)
+                {
+                    buf = buf + bytes_str[1][((i - 1) * 3 + j) - 1];
+                }
+                textBox2.Text = textBox2.Text + buf + "\r\n";
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                xx[1, i] = Convert.ToDouble(char.GetNumericValue(bytes_str[1][i]));
+            }
+
+            for (i = 1; i < 6; i++)
+            {
+                buf = "";
+                for (j = 1; j < 4; j++)
+                {
+                    buf = buf + bytes_str[2][((i - 1) * 3 + j) - 1];
+                }
+                textBox3.Text = textBox3.Text + buf + "\r\n";
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                xx[2, i] = Convert.ToDouble(char.GetNumericValue(bytes_str[2][i]));
+            }
+
+            for (i = 1; i < 6; i++)
+            {
+                buf = "";
+                for (j = 1; j < 4; j++)
+                {
+                    buf = buf + bytes_str[3][((i - 1) * 3 + j) - 1];
+                }
+                textBox4.Text = textBox4.Text + buf + "\r\n";
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                xxn[0, i] = Convert.ToDouble(char.GetNumericValue(bytes_str[3][i]));
+            }
+
+            for (i = 1; i < 6; i++)
+            {
+                buf = "";
+                for (j = 1; j < 4; j++)
+                {
+                    buf = buf + bytes_str[4][((i - 1) * 3 + j) - 1];
+                }
+                textBox5.Text = textBox5.Text + buf + "\r\n";
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                xxn[1, i] = Convert.ToDouble(char.GetNumericValue(bytes_str[4][i]));
+            }
+
+            for (i = 1; i < 6; i++)
+            {
+                buf = "";
+                for (j = 1; j < 4; j++)
+                {
+                    buf = buf + bytes_str[5][((i - 1) * 3 + j) - 1];
+                }
+                textBox6.Text = textBox6.Text + buf + "\r\n";
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                xxn[2, i] = Convert.ToDouble(char.GetNumericValue(bytes_str[5][i]));
+            }
+
+            //Делаем доступными наши кнопки
+            button_learn.Enabled = true;
+            button_recognition.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
